fdc3f1a877aaef9ea1cd2754542626f0efacee98
yuiiNidjari <5710110234@psu.ac.th>
2018-04-23 19:03:16 +0700

    list bears

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {data: []};
+  }
+  componentDidMount(){
+    axios.get('http://localhost:8000/api/bears')
+    .then(result => {
+      this.setState({data: result.data})
+    })
+  }
   render() {
+    let bears = this.state.data;
     return (
-      <div>Hello World</div>
+      <div>
+        {
+          bears.map(bear => <div key={bear.id}>{bear.name}</div>)
+        }
+      </div>
     );
   }
 }
